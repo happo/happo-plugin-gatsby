dfeb6e715075e1c4bd3f8393951ae5510db94997
Henric Trotzig <henric.trotzig@gmail.com>
2018-09-10 19:46:02 +0200

    Prepare for release
    Added tests to get some basic test coverage. Found a few issues in the
    process.
    
    Turned allPages into pageFilter for better user control.
    
    Added publicFolder to allow overriding ./public. Useful in tests at
    least!

--- README.md
+++ README.md
@@ -17,8 +17,54 @@ module.exports = {
       // options go here
     }),
   ],
+
+  type: 'plain',
 }
+```
+
+The Gatsby plugin for Happo takes screenshots on pre-built Gatsby pages.
+Therefore, before you run `happo run` you need to run a Gatsby build. Use the
+[`INSTALL_CMD` environment
+variable](https://github.com/enduire/happo.io#integrating-with-your-continuous-integration-ci-environment)
+to tell Happo to build before screenshooting.  This could look something like
+this (using Travis CI as an example):
+
+```yaml
+# .travis.yml
+script:
+- INSTALL_CMD="yarn build" yarn happo-ci
 ```
 
 ## Options
 
+### `pageFilter`
+
+By default, all Gatsby pages are included in the test suite. By providing a
+filter function, you can exclude certain pages. Here's an example excluding all
+"blog" pages:
+
+```js
+happoPluginGatsby({
+  pageFilter: (pathToFile) => {
+    if (/\/blog/.test(pathToFile)) {
+      // exclude blog pages
+      return false;
+    }
+    return true;
+  },
+});
+```
+
+### `publicFolder`
+
+Use this option to override where Happo looks for pre-built Gatsby pages. The
+default is `path.resolve(process.cwd(), 'public')`. Make sure to use an
+absolute file path here.
+
+```js
+const path = require('path');
+
+happoPluginGatsby({
+  publicFolder: path.resolve(__dirname, 'custom-build'),
+});
+```
--- index.js
+++ index.js
@@ -3,43 +3,37 @@ const path = require('path');
 
 const glob = require('glob');
 const webpack = require('webpack');
-const { RemoteBrowserTarget } = require('happo.io');
 
 const FILENAME_PATTERN = /\/([\w-_]+)\/index\.html?$/;
 
-function filenameToComponentName(filename) {
-  return filename.replace(`${process.cwd()}/public/`, '').replace('/index.html', '') || 'index';
+function filenameToComponentName({ filename, publicFolder }) {
+  return filename.replace(`${publicFolder}/`, '').replace(/\/?index.html/, '') || 'index';
 }
 
-module.exports = function happoPluginGatsby({ allPages = true } = {}) {
+module.exports = function happoPluginGatsby({
+  pageFilter = () => true,
+  publicFolder = path.resolve(process.cwd(), 'public'),
+} = {}) {
   return {
     customizeWebpackConfig: config => {
-      let pages = glob.sync(path.join(__dirname, './public/**/index.html'));
-      if (!pages.length) {
-        throw new Error('No `index.html` files were found in the ./public folder. Make sure you run `gatsby build` first.');
+      const pageFilenames = glob.sync(path.join(publicFolder, '/**/index.html')).filter(pageFilter);
+      if (!pageFilenames.length) {
+        throw new Error(
+          `No \`index.html\` files were found in ${publicFolder}. Make sure you run \`gatsby build\` first.`,
+        );
       }
-      const seenDirs = new Set();
 
-      if (!allPages) {
-        // Filter out "duplicate" pages (those that are variants of each other).
-        pages = pages.filter(page => {
-          const dir = path.dirname(path.dirname(page));
-          if (seenDirs.has(dir)) {
-            return false;
-          }
-          seenDirs.add(dir);
-          return true;
-        });
-      }
-      console.log(`Found ${pages.length} Gatsby pages`);
-      config.resolve.extensions = ['', '.js', '.jsx', '.json'];
+      console.log(`Found ${pageFilenames.length} Gatsby pages`);
 
       config.plugins.push(
         new webpack.DefinePlugin({
           HAPPO_GATSBY_PAGES: JSON.stringify(
-            pages.map(page => ({
-              component: filenameToComponentName(page),
-              content: fs.readFileSync(page, 'utf-8'),
+            pageFilenames.map(filename => ({
+              component: filenameToComponentName({
+                filename,
+                publicFolder,
+              }),
+              content: fs.readFileSync(filename, 'utf-8'),
             })),
           ),
         }),
@@ -47,8 +41,7 @@ module.exports = function happoPluginGatsby({ allPages = true } = {}) {
       return config;
     },
 
-    publicFolders: [path.resolve(process.cwd(), 'public')],
-
-    pathToExamplesFile: path.join(__dirname, 'happoExamples'),
+    publicFolders: [publicFolder],
+    pathToExamplesFile: path.join(__dirname, 'happoExamples.js'),
   };
 };
